65ac20b0d676fe407661e3c9dd91cab3d0413726
Sam Gwilliam <sambc49@gmail.com>
2019-01-20 15:26:29 +0000

    Initial check-in of module CSharpChallenges

---
+++ CSharpChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
